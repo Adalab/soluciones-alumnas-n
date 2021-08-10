1961a6dde7015f08932aab98d5e5da82d942dd57
AndreaAguado <andrea95am@gmail.com>
2021-08-10 20:33:19 +0200

    add exercise 5 from module 2 lesson 5

---
+++ modulo-2-leccion-05-ejercicio-05-Andrea/js/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const background = document.querySelector('.main');
+
+
+function changeColor (){
+    if(background.classList.contains('purple')){
+        background.classList.remove('purple');
+        background.classList.add('red');
+    }
+    else if (background.classList.contains('red')){
+        background.classList.remove('red');
+        background.classList.add('purple');
+    }
+}
+
+document.addEventListener('keydown', changeColor);
+
+// IMPORTANTE no se pone en este caso el elemento background sino que se hace directamente sobre document
