c2882e6badd475ef571a49e12135f1a031f06182
JuditAldeguer <jud.vicens@hotmail.es>
2021-09-07 17:21:21 +0200

    ex Jud

--- Module-2/modulo-2-lección-13-ejercicio-6&7/js/main.js
+++ Module-2/modulo-2-lección-13-ejercicio-6&7/js/main.js
@@ -15,3 +15,12 @@ for (let i = 0; i < usersAcces.length; i++) {
     `Este usuario tiene un PIN par ${usersAcces[i].name} y, por tanto, acceso.`
   );
 }
+
+//Ejercio 7
+const times = [56, 9, 45, 28, 35];
+const timesResult = times.reduce((acc, time) => {
+  const total = acc + time;
+  return total;
+});
+console.log(times.length);
+console.log(timesResult / times.length); // deberia ser 34,6
